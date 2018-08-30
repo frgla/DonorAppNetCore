d797deea871cb470b1eb4f8fea862896a5042881
Fran Glavota <f.glavota@gmail.com>
2018-08-30 23:16:21 +0200

    Home and register

---
+++ DonorApp-SPA/src/app/register/tempCodeRunnerFile.ts
@@ -0,0 +1,3 @@
+ register() {
+    console.log('cancelled');
+  }
--- DonorApp.API/Controllers/ValuesController.cs
+++ DonorApp.API/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace DonorApp.Api.Controllers
             _context = context;
 
         }
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
